f3ae4fbb59387febee21a9ee84acdc984f4b0198
Pradeep Murugesan <pradeep.murugesan@scout24.com>
2018-10-08 19:29:33 +0530

    added coverage for encrypt, decrypt and reset

--- test/unit/specs/components/PasswordManager.spec.js
+++ test/unit/specs/components/PasswordManager.spec.js
@@ -17,4 +17,56 @@ describe('PasswordManager.vue', () => {
     expect(wrapper.findAll('button').length).to.equal(3)
     expect(wrapper.findAll('input').length).to.equal(2)
   })
+
+  it('can encrypt the data', () => {
+    const wrapper = mount(PasswordManager, { stubs: {
+      'ExisingPasswords': true
+    }})
+
+    const plainText = wrapper.find('#plainText')
+    const secret = wrapper.find('#secret')
+    const encryptButton = wrapper.find('.btn-success')
+
+    plainText.setValue('Hello')
+    secret.setValue('World')
+    encryptButton.trigger('click')
+
+    expect(wrapper.vm.$data.result).to.equal('ed03dd86e7')
+    expect(wrapper.vm.$data.encrypted).to.equal(true)
+  })
+
+  it('can decrypt the data', () => {
+    const wrapper = mount(PasswordManager, { stubs: {
+      'ExisingPasswords': true
+    }})
+
+    const plainText = wrapper.find('#plainText')
+    const secret = wrapper.find('#secret')
+    const decryptButton = wrapper.find('.btn-primary')
+
+    plainText.setValue('ed03dd86e7')
+    secret.setValue('World')
+    decryptButton.trigger('click')
+
+    expect(wrapper.vm.$data.result).to.equal('Hello')
+    expect(wrapper.vm.$data.encrypted).to.equal(false)
+  })
+
+  it('should reset the fields', () => {
+    const wrapper = mount(PasswordManager, { stubs: {
+      'ExisingPasswords': true
+    }})
+
+    const plainText = wrapper.find('#plainText')
+    const secret = wrapper.find('#secret')
+    const resetButton = wrapper.find('.btn-danger')
+
+    plainText.setValue('ed03dd86e7')
+    secret.setValue('World')
+    resetButton.trigger('click')
+
+    expect(wrapper.vm.$data.result).to.equal('')
+    expect(wrapper.vm.$data.plain).to.equal('')
+    expect(wrapper.vm.$data.secret).to.equal('')
+  })
 })
